32bbce7c5f43c215a92277796107f3d5910b08ac
huaipro <164613353+lihuai0416@users.noreply.github.com>
2024-11-23 01:57:18 +0800

    Delete python_snake/pizza.py

--- python_snake/pizza.py
+++
@@ -1,5 +0,0 @@
-# 此py文件用于8
-def make_pizza(size,*toppings):
-    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
-    for topping in toppings:
-        print("- " + topping)
